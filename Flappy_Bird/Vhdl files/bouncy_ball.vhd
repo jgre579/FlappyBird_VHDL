3dc6c4b510de024d504a30871ad0f84d4d66be66
wynnmo <wmo550@aucklanduni.ac.nz>
2022-05-28 09:45:43 +1200

    created level counter during normal mode. level counter increases as player gets more points

--- Flappy_Bird/Vhdl files/bouncy_ball.vhd
+++ Flappy_Bird/Vhdl files/bouncy_ball.vhd
@@ -32,7 +32,7 @@ size <= CONV_STD_LOGIC_VECTOR(8,10);
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
 ball_x_pos <= CONV_STD_LOGIC_VECTOR(300,11);
 
-ball_on <= '1' when ( (text_mode = "000" or text_mode = "001" or text_mode = "010") and ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
+ball_on <= '1' when ((text_mode = "000" or text_mode = "001") and ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
 					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size))  else	-- y_pos - size <= pixel_row <= y_pos + size
 			'0' ;
 
--- Flappy_Bird/Vhdl files/level_control.vhd
+++ Flappy_Bird/Vhdl files/level_control.vhd
@@ -10,12 +10,15 @@ ENTITY level_control
 				
 		score_tens, score_hundreds	: in std_logic_vector(5 downto 0);
 		game_mode, clk : in std_logic;
-		speed : out std_logic_vector(9 DOWNTO 0)
+		speed : out std_logic_vector(9 DOWNTO 0);
+		level_ones,level_tens : out std_logic_vector(5 downto 0)
 		
 		);		
 END level_control;
 
 architecture behavior of level_control is
+
+	signal current_level_ones, current_level_tens	: std_logic_vector(5 DOWNTO 0) := "110000";  
 
 
 BEGIN    
@@ -25,11 +28,11 @@ process  is
 
 
 begin 
+	
 	wait until rising_edge(clk);
 	
 	-- TRAINING MODE
 	if(game_mode = '0') then 
-	
 		speed <= CONV_STD_LOGIC_VECTOR(2, 10);
 		
 	else 
@@ -43,13 +46,25 @@ begin
 			when others => speed <= CONV_STD_LOGIC_VECTOR(6, 10);
 		end case;
 		end if;
+		
 	end if;
-		
-		
-		
-		
-			
-		
+	
+
+	--CALCULATE LEVEL 
+	if (score_tens > current_level_ones)then 
+		current_level_ones <= current_level_ones + CONV_STD_LOGIC_VECTOR(1, 6);
+		elsif(score_tens = "110000")then
+		current_level_ones <= "110000";
+	end if; 
+	
+	if (score_hundreds > current_level_tens)then 
+		current_level_tens <= current_level_tens + CONV_STD_LOGIC_VECTOR(1, 6);
+		elsif(score_hundreds = "110000") then 
+		current_level_tens <= "110000";
+	end if; 
+	
+level_ones <= current_level_ones;
+level_tens <= current_level_tens;
 
 end process;
 
