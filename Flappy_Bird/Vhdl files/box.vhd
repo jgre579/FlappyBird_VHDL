130317eeec7d9ae5ad5b3fb12962649e71b2dd9a
jgre579 <jgre579@aucklanduni.ac.nz>
2022-05-29 16:52:14 +1200

    Clean up bouncy box and pip_gen

---
+++ Flappy_Bird/Vhdl files/box.vhd
@@ -0,0 +1,86 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+USE  IEEE.STD_LOGIC_ARITH.all;
+USE  IEEE.STD_LOGIC_SIGNED.all;
+
+
+ENTITY box IS
+	PORT
+		(
+		vert_sync, mouse_click, enable, reset						: IN std_logic;	
+		text_mode															: IN std_logic_vector(2 downto 0);
+      pixel_row, pixel_column											: IN std_logic_vector(9 DOWNTO 0);
+		box_on, touched_bot												: OUT std_logic);
+		  		
+END box;
+
+architecture behavior of box is
+
+
+SIGNAL size 											: std_logic_vector(9 DOWNTO 0);  
+SIGNAL box_y_pos										: std_logic_vector(9 DOWNTO 0);
+SiGNAL box_x_pos										: std_logic_vector(10 DOWNTO 0);
+SIGNAL box_y_motion									: std_logic_vector(9 DOWNTO 0);
+
+
+BEGIN           
+
+
+
+size <= CONV_STD_LOGIC_VECTOR(8,10);
+box_x_pos <= CONV_STD_LOGIC_VECTOR(300,11);
+
+box_on <= '1' when ((text_mode = "000" or text_mode = "001") and ('0' & box_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & box_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & box_y_pos <= pixel_row + size) and ('0' & pixel_row <= box_y_pos + size))  else	-- y_pos - size <= pixel_row <= y_pos + size
+			'0' ;
+
+			
+Move_Box: process (vert_sync) 
+variable ball_hit_top : std_logic := '0'; 	 
+begin
+
+	-- Move box only on vertical sync 
+	if (rising_edge(vert_sync)) then	
+		
+		if(reset = '1') then 
+			-- Reset boxes position so don't auto die on respawn
+			box_y_pos <= CONV_STD_LOGIC_VECTOR(200,10);
+			touched_bot <= '0';
+		end if;
+		
+		if(enable = '1') then 
+			
+			if ((box_y_pos >= CONV_STD_LOGIC_VECTOR(479,10)) ) then
+				-- If box hits bottom of the screen
+				touched_bot <= '1';
+			elsif (box_y_pos <= CONV_STD_LOGIC_VECTOR(2,10)) then
+				-- If ball hits top of the screen 
+				box_y_pos <= CONV_STD_LOGIC_VECTOR(0,10);
+				ball_hit_top := '1';
+			else 
+				ball_hit_top := '0';
+				
+			end if;
+			
+			
+			
+			--Apply Gravity	
+			if(mouse_click = '1' and ball_hit_top = '0') then 
+				--move upward on click
+					box_y_motion <= - CONV_STD_LOGIC_VECTOR(9,10);
+			else 
+				-- Otherwise fall down 
+				box_y_motion <= CONV_STD_LOGIC_VECTOR(2,10);
+			end if;
+			
+			-- Compute next ball Y position
+			box_y_pos <= box_y_pos + box_y_motion;
+			
+			
+			end if;	
+		
+	end if;
+end process Move_Box;
+
+END behavior;
+
--- Flappy_Bird/Vhdl files/pipe_gen.vhd
+++ Flappy_Bird/Vhdl files/pipe_gen.vhd
@@ -1,3 +1,4 @@
+
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.all;
 USE  IEEE.STD_LOGIC_ARITH.all;
@@ -6,12 +7,12 @@ USE  IEEE.STD_LOGIC_SIGNED.all;
 
 ENTITY pipe_gen IS
 	PORT
-		(clk, vert_sync, enable, reset, game_over : IN std_logic;
-         pixel_row, pixel_column: IN std_logic_vector(9 DOWNTO 0);
-			rand_num_out : IN std_logic_vector(7 DOWNTO 0);
-			speed : in std_logic_vector(9 DOWNTO 0);
-			pipe_on, next_pipe_on, pipe_passed : OUT std_logic;
-			pipe_position :	OUT std_logic_vector(9 DOWNTO 0));		
+		(clk, vert_sync, enable, reset 			: IN std_logic;
+         pixel_row, pixel_column					: IN std_logic_vector(9 DOWNTO 0);
+			rand_num_out 								: IN std_logic_vector(7 DOWNTO 0);
+			speed 										: IN std_logic_vector(9 DOWNTO 0);
+			pipe_on, next_pipe_on, pipe_passed 	: OUT std_logic
+			);		
 END pipe_gen;
 
 architecture behavior of pipe_gen is
@@ -28,19 +29,18 @@ SIGNAL pipe_fade_x			: std_logic_vector(9 DOWNTO 0);
 
 BEGIN           
 
-pipe_position <= pipe_width; 
--- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
-
--- pipe 1  ___________________________________________________________________
 
 
 
-pipe_top_on <= '1' when ( ('0' & pixel_column >= '0' & pipe_x_pos) and ('0' & pixel_column <= '0' & pipe_x_pos + pipe_width) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & pixel_row <= pipe_top_y_pos))  else	-- y_pos - size <= pixel_row <= y_pos + size
+
+---- Pipe coordinates start at its top lefts hadn corner to simplifiy calculations. 
+
+pipe_top_on <= '1' when ( ('0' & pixel_column >= '0' & pipe_x_pos) and ('0' & pixel_column <= '0' & pipe_x_pos + pipe_width) 	-- x_pos <= pixel_column <= x_pos + size
+					and ('0' & pixel_row <= pipe_top_y_pos))  else	-- y_pos <= pixel_row <= y_pos + size
 			'0';
 
-pipe_bot_on <= '1' when ( ('0' & pixel_column >= '0' & pipe_x_pos) and ('0' & pixel_column <= '0' & pipe_x_pos + pipe_width) 	-- x_pos - size <= pixel_column <= x_pos + size
-					and ('0' & pipe_bot_y_pos <= pixel_row)) else	-- y_pos - size <= pixel_row <= y_pos + size
+pipe_bot_on <= '1' when ( ('0' & pixel_column >= '0' & pipe_x_pos) and ('0' & pixel_column <= '0' & pipe_x_pos + pipe_width) 	-- x_pos <= pixel_column <= x_pos + size
+					and ('0' & pipe_bot_y_pos <= pixel_row)) else	-- y_pos <= pixel_row <= y_pos + size
 			'0';
 
 pipe_on <= (pipe_bot_on or pipe_top_on);
@@ -54,48 +54,52 @@ variable v_pipe_passed : std_logic := '0';
 
 begin
 	-- Move pipes once every vert_sync 
-	-- will only move if switch is enabled
 	if (rising_edge(vert_sync)) then 
 					
 		if(enable = '1') then 
-			
+			-- On startup or when pipes are fully off the screen, get another random number for pos'
 			if(pipe_width <= CONV_STD_LOGIC_VECTOR(0, 11) or pipe_bot_y_pos = "0000000000") then 
 			
 				pipe_bot_y_pos <=("00" & rand_num_out) - "110010000";
 				pipe_top_y_pos <=("00" & rand_num_out);
 			
 			end if;
-		
-		
-			if(count = 1) then -- moving pipes once
+			
+			if(count = 1) then
 				
 				pipe_x_motion <= speed;
 				
 				count := 0;
-			else -- resetting pipe count after moving 
+			else 
 				pipe_x_motion <= CONV_STD_LOGIC_VECTOR(0,10);
 				count := count + 1;
 			end if;
+			
 		
 		
 --		pipe1  ___________________________________________________________________
 			if(pipe_x_pos <= CONV_STD_LOGIC_VECTOR(0, 10)) then 
 				
-					
+				-- If pipe width is now 0 	
 				if(pipe_width <= CONV_STD_LOGIC_VECTOR(0, 10)) then 
+					-- Reset pipes position and width 
 					pipe_x_pos <= CONV_STD_LOGIC_VECTOR(640,11);
 					pipe_width <= CONV_STD_LOGIC_VECTOR(50, 10);
 				
 				else 
+					-- When x_pos = 0, shrink pipe width to gice the illusion of the pipe disappearing off screen
 					pipe_width <= pipe_width - pipe_x_motion;
 				end if;
 				
 				
 			else 
+				-- Move pipes normally
 				pipe_x_pos <= pipe_x_pos - pipe_x_motion;
 				pipe_width <= CONV_STD_LOGIC_VECTOR(50, 10);
 			end if;
-		
+			
+
+			-- Use pipe postion for the emmision of game signals.
 			if(pipe_x_pos = CONV_STD_LOGIC_VECTOR(424, 11))then
 				v_next_pipe_on := '1';
 			else 
